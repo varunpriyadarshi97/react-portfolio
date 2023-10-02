4098b24eeb054351451365e75caf227b4be61adc
Varun Priyadarshi <86125404+varunpriyadarshi97@users.noreply.github.com>
2023-10-02 22:06:23 +0000

    First Commit

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
